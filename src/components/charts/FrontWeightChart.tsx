412923be53e9f47b32bb00ffcf40fd5e303d8ac6
Matthieu Huguet <matthieu@huguet.eu>
2025-08-15 19:24:01 +0200

    ✨ add front weight chart on the design page with recommended and measured values

--- src/components/charts/FrontWeightChart.tsx
+++ src/components/charts/FrontWeightChart.tsx
@@ -6,12 +6,29 @@ import {
   TouchDesignSerieVariant,
   type TouchDesignSerie,
 } from './TouchDesignChart';
+import { usePianoMeasures } from '@/hooks/use-measure-store';
+import { useMemo } from 'react';
 
 export interface FrontWeightChartProps {
   defaultFrontWeightLevelsToInclude?: FrontWeightLevel[];
 }
 
 export const FrontWeightChart = (props: FrontWeightChartProps) => {
+  const measures = usePianoMeasures();
+  const measuredSerie: TouchDesignSerie = useMemo(() => {
+    return {
+      data:
+        measures.keys?.map((key) =>
+          key.frontWeight ? Number(key.frontWeight) : undefined,
+        ) ?? [],
+      name: 'Measured',
+      variant: TouchDesignSerieVariant.Measured,
+    };
+  }, [measures]);
+  const shouldDisplayMeasuredSerie = useMemo(() => {
+    return measuredSerie.data.some((data) => data !== undefined);
+  }, [measuredSerie]);
+
   const seriesWithBoldVariant = [
     FrontWeightLevel.Level5,
     FrontWeightLevel.Level7,
@@ -26,15 +43,18 @@ export const FrontWeightChart = (props: FrontWeightChartProps) => {
     FrontWeightLevel.Level9,
   ];
 
-  const series: TouchDesignSerie[] = defaultSeriesToInclude.map(
-    (fwLevel): TouchDesignSerie => ({
-      data: frontWeightData[fwLevel],
-      name: `${fwLevel}`,
-      variant: seriesWithBoldVariant.includes(fwLevel)
-        ? TouchDesignSerieVariant.DefaultBold
-        : TouchDesignSerieVariant.Default,
-    }),
-  );
+  const series: TouchDesignSerie[] = [
+    ...defaultSeriesToInclude.map(
+      (fwLevel): TouchDesignSerie => ({
+        data: frontWeightData[fwLevel],
+        name: `${fwLevel}`,
+        variant: seriesWithBoldVariant.includes(fwLevel)
+          ? TouchDesignSerieVariant.DefaultBold
+          : TouchDesignSerieVariant.Default,
+      }),
+    ),
+    ...(shouldDisplayMeasuredSerie ? [measuredSerie] : []),
+  ].filter((serie) => serie !== undefined);
 
   return <TouchDesignChart series={series} yAxisName="Front Weight" />;
 };
